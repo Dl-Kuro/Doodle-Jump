eca713918c8adba9acf234f36920c0608303e01f
Kuro <kw779966@gmail.com>
2023-02-26 17:42:59 +0100

    refactor

--- js/main.js
+++ js/main.js
@@ -1,100 +1,121 @@
-const DOODLE_SPEED = 40;
-const DOODLE_SIZE = 60;
+const DOODLER = {
+    SPEED: 10,
+    SIZE: 60,
+};
 
-const PLATFORM_WIDTH = 100;
-const PLATFORM_HEIGHT = 20;
-const NUM_PLATFORMS = 10;
+const PLATFORM = {
+    WIDTH: 100,
+    HEIGHT: 20,
+    NUM: 10,
+};
 
 const GAME_RECT = document.getElementById("game").getBoundingClientRect();
-const GAME_WIDTH = GAME_RECT.width;
-const GAME_HEIGHT = GAME_RECT.height;
-const GAME_POS_ZERO = 0;
+const GAME = {
+    WIDTH: GAME_RECT.width,
+    HEIGHT: GAME_RECT.height,
+    POS_ZERO: 0,
+}
 console.log(GAME_RECT);
 
 class Element {
+    #status = true; // not destroyed
+    #width;
+    #height;
+    #pos;
+    #speed;
+
     constructor(width, height, px, py, speed) {
-        this._status = true;    // not destroyed
-        this._width = width;
-        this._height = height;
-        this._pos = {x: px, y: py};
-        this._speed = speed;
-    }
-
-    get speed() {
-        return this._speed;
-    }
-
-    get status() {
-        return this._status;
+        this.#width = width;
+        this.#height = height;
+        this.#pos = { x: px, y: py };
+        this.#speed = speed;
     }
 
     get width() {
-        return this._width;
+        return this.#width;
+    }
+
+    set width(value) {
+        this.#width = value;
     }
 
     get height() {
-        return this._height;
+        return this.#height;
+    }
+
+    set height(value) {
+        this.#height = value;
+    }
+
+    get speed() {
+        return this.#speed;
+    }
+
+    set speed(value) {
+        this.#speed = value;
+    }
+
+    get status() {
+        return this.#status;
+    }
+
+    set status(value) {
+        this.#status = value;
     }
 
     get pos() {
-        return this._pos;
+        return this.#pos;
+    }
+
+    set pos(value) {
+        this.#pos = value;
     }
 }
 
 class Doodler extends Element {
+    #velocity = 0;
+    #gravity = 0.2;
+
     constructor(width, height, x, y, speed) {
         super(width, height, x, y, speed);
-        this._width = width;
-        this._height = height;
-        this._velocity = 0;
         document.addEventListener("keydown", this.moveDoodler.bind(this));
-    }
-
-    get width() {
-        return this._width;
-    }
-
-    get height() {
-        return this._height;
     }
 
     moveDoodler(event) {
         if (event.keyCode === 37) {
-            this._pos.x -= this._speed;
+            this.pos.x -= this.speed;
         } else if (event.keyCode === 39) {
-            this._pos.x += this._speed;
+            this.pos.x += this.speed;
         }
 
-        if (this._pos.x > (GAME_WIDTH)) {
+        if (this.pos.x > (GAME.WIDTH)) {
             this.setDefaultLeft();
             window.requestAnimationFrame(this.moveDoodler.bind(this))
-        } else if (this._pos.x < GAME_POS_ZERO) {
+        } else if (this.pos.x < GAME.POS_ZERO) {
             this.setDefaultRight();
             window.requestAnimationFrame(this.moveDoodler.bind(this))
         }
     }
 
     setDefaultLeft() {
-        this._pos.x = GAME_POS_ZERO;
+        this.pos.x = GAME.POS_ZERO;
     }
 
     setDefaultRight() {
-        this._pos.x = GAME_WIDTH - this._width;
+        this.pos.x = GAME.WIDTH - this.width;
     }
 
     update() {
-        this._velocity += 0.2;
-        this._pos.y += this._velocity;
+        this.#velocity += this.#gravity ;
+        this.pos.y += this.#velocity;
     }
 
     checkCollision(platform) {
-        console.log(this._velocity)
-
-        if (this._pos.x < platform.pos.x + platform.width && this._pos.x + this._width > platform.pos.x &&
-            this._pos.y < platform.pos.y - platform.height * 2 && this._pos.y + this._height > platform.pos.y &&
-            this._velocity > 0)
+        if (this.pos.x < platform.pos.x + platform.width && this.pos.x + this.width > platform.pos.x &&
+            this.pos.y < platform.pos.y - platform.height * 2 && this.pos.y + this.height > platform.pos.y &&
+            this.#velocity > 0)
         {
-            this._velocity = -10;
+            this.#velocity = -10;
         }
     }
 }
@@ -107,86 +128,77 @@ class Platform extends Element {
 
 
 class Model {
-    constructor() {
-        this._doodler = null;
-        this._platforms = [];
-        this._monsters = [];
-    }
+    #doodler = null;
+    #platforms = [];
+    #monsters = [];
 
     init() {
-        this._doodler = new Doodler(DOODLE_SIZE, DOODLE_SIZE, (GAME_WIDTH / 2 - DOODLE_SIZE / 2), (GAME_HEIGHT - (DOODLE_SIZE + PLATFORM_HEIGHT)), DOODLE_SPEED);
+        this.#doodler = new Doodler(DOODLER.SIZE, DOODLER.SIZE, (GAME.WIDTH / 2 - DOODLER.SIZE / 2), (GAME.HEIGHT - (DOODLER.SIZE + PLATFORM.HEIGHT)), DOODLER.SPEED);
 
-        let startingPlatformX = GAME_WIDTH / 2 - PLATFORM_WIDTH / 2;
-        let startingPlatformY = GAME_HEIGHT - PLATFORM_HEIGHT;
-        this._platforms.push(new Platform(PLATFORM_WIDTH, PLATFORM_HEIGHT, startingPlatformX, startingPlatformY, 0));
+        let startingPlatformX = GAME.WIDTH / 2 - PLATFORM.WIDTH / 2;
+        let startingPlatformY = GAME.HEIGHT - PLATFORM.HEIGHT;
+        this.#platforms.push(new Platform(PLATFORM.WIDTH, PLATFORM.HEIGHT, startingPlatformX, startingPlatformY, 0));
 
-        for (let i = 0; i < NUM_PLATFORMS; i++) {
-            let randomX = Math.random() * (GAME_WIDTH - PLATFORM_WIDTH);
-            let randomY = Math.random() * (GAME_HEIGHT - PLATFORM_HEIGHT);
-            this._platforms.push(new Platform(PLATFORM_WIDTH, PLATFORM_HEIGHT, randomX, randomY, 0));
-        }
-    }
-
-    update() {
-        this._doodler.update();
-
-        for (let i = 0; i < this._platforms.length; i++) {
-            this._doodler.checkCollision(this._platforms[i]);
-        }
-
-        if (this._doodler.pos.y > GAME_HEIGHT) {
-            this._doodler._status = false;
+        for (let i = 0; i < PLATFORM.NUM; i++) {
+            let randomX = Math.random() * (GAME.WIDTH - PLATFORM.WIDTH);
+            let randomY = Math.random() * (GAME.HEIGHT - PLATFORM.HEIGHT);
+            this.#platforms.push(new Platform(PLATFORM.WIDTH, PLATFORM.HEIGHT, randomX, randomY, 0));
         }
     }
 
     get doodler() {
-        return this._doodler;
+        return this.#doodler;
+    }
+
+    set doodler(value) {
+        this.#doodler = value;
     }
 
     get platforms() {
-        return this._platforms;
+        return this.#platforms;
+    }
+
+    set platforms(value) {
+        this.#platforms = value;
     }
 
     get monsters() {
-        return this._monsters;
+        return this.#monsters;
+    }
+
+    set monsters(value) {
+        this.#monsters = value;
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class View {
-    constructor() {
-        this._game = document.querySelector("#game");
-        this._template = document.querySelector("#template");
-    }
+    #game = document.querySelector("#game");
+    #template = document.querySelector("#template");
 
     render(model) {
-        this._game.innerHTML = "";
-
-        let cloneTemplate = document.importNode(this._template.content, true);
+        this.#game.innerHTML = "";
+        let cloneTemplate = document.importNode(this.#template.content, true);
 
         if (model.doodler.status) {
             let doodler = model.doodler
             let elemDoodler = cloneTemplate.querySelector(".defaultDoodler");
-
             elemDoodler.style.marginLeft = doodler.pos.x + "px";
             elemDoodler.style.marginTop = doodler.pos.y + "px";
             elemDoodler.style.width = doodler.width + "px";
             elemDoodler.style.height = doodler.height + "px";
-
-            this._game.appendChild(elemDoodler);
+            this.#game.appendChild(elemDoodler);
         }
 
         model.platforms.forEach(platform => {
-            let cloneTemplate = document.importNode(this._template.content, true);
+            let cloneTemplate = document.importNode(this.#template.content, true);
             let elemPlatform = cloneTemplate.querySelector(".defaultPlatform");
-
             elemPlatform.style.marginLeft = platform.pos.x + "px";
             elemPlatform.style.marginTop = platform.pos.y + "px";
             elemPlatform.style.width = platform.width + "px";
             elemPlatform.style.height = platform.height + "px";
-
-            this._game.appendChild(elemPlatform);
+            this.#game.appendChild(elemPlatform);
         })
     }
 }
@@ -194,24 +206,60 @@ class View {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Controller {
+    #model = model;
+    #view = view;
+    #fps = 120;
+    #interval = 1000 / this.#fps;
+
     constructor(model, view) {
-        this._model = model;
-        this._view = view;
-        this._fps = 120;
-        this._interval = 1000 / this._fps;
+        this.#model = model;
+        this.#view = view;
     }
 
     start() {
-        this._model.init();
+        this.#model.init();
         window.requestAnimationFrame(this.step.bind(this));
     }
 
     step() {
-        this._model.update();
-        this._view.render(this._model);
+        this.loop();
+        this.#view.render(this.#model);
         setTimeout(() => {
-            window.requestAnimationFrame(this.step.bind(this));
-        }, this._interval);
+            if (!this.isGameOver()) {
+                window.requestAnimationFrame(this.step.bind(this));
+            }
+        }, this.#interval);
+    }
+
+    loop() {
+        this.checkScroll();
+        this.#model.doodler.update();
+
+        for (let i = 0; i < this.#model.platforms.length; i++) {
+            this.#model.doodler.checkCollision(this.#model.platforms[i]);
+        }
+
+        if (this.#model.doodler.pos.y > GAME.HEIGHT) {
+            this.#model.doodler.status = false;
+        }
+    }
+
+    isGameOver() {
+        if (!this.#model.doodler.status) {
+            console.log("Game Over")
+            return true
+        }
+        return false
+    }
+
+    checkScroll() {
+        if (this.#model.doodler.pos.y < GAME.HEIGHT / 2) {
+            let scrollY = GAME.HEIGHT / 2 - this.#model.doodler.pos.y;
+            this.#model.platforms.forEach(platform => {
+                platform.pos.y += scrollY;
+            });
+            this.#model.doodler.pos.y += scrollY;
+        }
     }
 }
 
